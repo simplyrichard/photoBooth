1ab0e43ba7abd6e040035b47ca682c24e95d60d7
Chris Vasey <chrismvasey@gmail.com>
2016-01-02 14:09:10 +0000

    updated comments

--- capture.sh
+++ capture.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Capture current time for timestamp
 DATE=$(date +"%Y-%m-%d_%H%M")
 
+#Take photo using webcam and save with timestamp as name
 fswebcam -r 1280x720 --no-banner /var/www/webcam/$DATE.jpg
